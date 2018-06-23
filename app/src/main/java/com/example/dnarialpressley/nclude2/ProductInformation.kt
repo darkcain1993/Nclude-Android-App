f7bcc5a35a8f378a024aa8cabfbe8df7ce7c0eb7
D'Narial Brown-Pressley <40445074+darkcain1993@users.noreply.github.com>
2018-06-23 10:09:52 -0700

    Update ProductInformation.kt

--- app/src/main/java/com/example/dnarialpressley/nclude2/ProductInformation.kt
+++ app/src/main/java/com/example/dnarialpressley/nclude2/ProductInformation.kt
@@ -1,9 +1,7 @@
-//© D'Narial Brown-Pressley All Rights Reserved.
+//© 2018 D'Narial Brown-Pressley All Rights Reserved.
 package com.example.dnarialpressley.nclude_a
 
-/**
- * Created by dnarial.pressley on 11/7/2017.
- */
+
 open class ProductInformation(){
     var productImage = 0
     var productName = ""
