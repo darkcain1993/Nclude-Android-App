b419d64de6984b64005a62565fa0a88d3ec5dbe5
D'Narial Brown-Pressley <40445074+darkcain1993@users.noreply.github.com>
2018-06-23 09:33:21 -0700

    Update Feedback.kt

--- app/src/main/java/com/example/dnarialpressley/nclude2/Feedback.kt
+++ app/src/main/java/com/example/dnarialpressley/nclude2/Feedback.kt
@@ -1,9 +1,7 @@
-//© D'Narial Brown-Pressley All Rights Reserved.
+//© 2018 D'Narial Brown-Pressley All Rights Reserved.
 package com.example.dnarialpressley.nclude_a
 
-/**
- * Created by D'Narial on 12/3/2017.
- */
+
 class Feedback() {
     var customerID = ""
     var customerEmail = ""
